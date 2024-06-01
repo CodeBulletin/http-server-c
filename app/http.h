b6a1e992fe64b8aa8b9a7ad23664668796ce6cd6
CodeBulletin <bmalhotra736@gmail.com>
2024-06-01 11:19:19 +0530

    codecrafters submit [skip ci]

---
+++ app/http.h
@@ -0,0 +1,79 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+#define MAX_HEADER_SIZE 1024
+#define MAX_BODY_SIZE 1024
+#define MAX_URL_SIZE 1024
+#define MAX_EXTRA_SIZE 512
+
+enum http_request_type {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    PATCH,
+    HEAD,
+    OPTIONS,
+    CONNECT,
+    TRACE
+};
+
+struct http_server
+{
+    int server_fd;
+    struct sockaddr_in serv_addr;
+    int connection_backlog;
+    int reuse;
+    uint16_t port;
+};
+
+
+struct http_header {
+    uint8_t *name;
+    size_t name_size;
+    uint8_t *value;
+    size_t value_size;
+};
+
+struct path {
+    uint8_t *name;
+    size_t name_size;
+};
+
+struct http_request {
+    enum http_request_type method;
+    uint8_t *url;
+    size_t url_size;
+    struct http_header *headers;
+    uint8_t *body;
+    size_t body_size;
+    struct path **path;
+    size_t path_size;
+};
+
+struct http_response {
+    uint16_t status_code;
+    struct http_header *headers;
+    size_t headers_size;
+    uint8_t *body;
+    size_t body_size;
+    uint8_t *msg;
+    size_t msg_size;
+};
+
+
+struct http_request *parse_http_request(uint8_t *buffer, size_t buffer_size);
+void free_http_request(struct http_request *req);
+void create_http_server(struct http_server *server);
+void free_http_server(struct http_server *server);
+struct http_response *create_http_response(uint16_t status_code, uint8_t *body, size_t body_size, struct http_header *headers, size_t headers_size, uint8_t *msg, size_t msg_size);
+void free_http_response(struct http_response *res);
+uint8_t* http_response_to_string(struct http_response *res);
--- app/server.c
+++ app/server.c
@@ -1,65 +1,62 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <string.h>
-#include <errno.h>
-#include <unistd.h>
+
+#include "http.c"
 
 int main() {
 	// Disable output buffering
 	setbuf(stdout, NULL);
 
+	// Ok Response
+	struct http_response *ok_res = create_http_response(200, "", 0, NULL, 0, "OK", 2);
+	uint8_t *ok_res_str = http_response_to_string(ok_res);
+	size_t ok_res_str_len = strlen(ok_res_str);
+	printf("%s\n", ok_res_str);
+	printf("%zu\n", ok_res_str_len);
+
+	// Not Found Response
+	struct http_response *not_found_res = create_http_response(404, "", 0, NULL, 0, "Not Found", 9);
+	uint8_t *not_found_res_str = http_response_to_string(not_found_res);\
+	size_t not_found_res_str_len = strlen(not_found_res_str);
+	printf("%s\n", not_found_res_str);
+	printf("%zu\n", not_found_res_str_len);
+
+
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	printf("Logs from your program will appear here!\n");
 
-	int server_fd, client_addr_len;
-	struct sockaddr_in client_addr;
-
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1) {
-		printf("Socket creation failed: %s...\n", strerror(errno));
-		return 1;
-	}
-
-	int reuse = 1;
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
-		printf("SO_REUSEPORT failed: %s \n", strerror(errno));
-		return 1;
-	}
-
-	struct sockaddr_in serv_addr = { 
-		.sin_family = AF_INET ,
-		.sin_port = htons(4221),
-		.sin_addr = { 
-			htonl(INADDR_ANY) 
-		},
+	struct http_server server = {
+		.port = 4221,
+		.connection_backlog = 5,
+		.reuse = 1,
 	};
 
-	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
-		printf("Bind failed: %s \n", strerror(errno));
-		return 1;
-	}
+	create_http_server(&server);
+	
+	struct sockaddr_in client_addr;
 
-	int connection_backlog = 5;
-	if (listen(server_fd, connection_backlog) != 0) {
-		printf("Listen failed: %s \n", strerror(errno));
-		return 1;
-	}
-	
 	printf("Waiting for a client to connect...\n");
-	client_addr_len = sizeof(client_addr);
+	int client_addr_len = sizeof(client_addr);
 	
-	int id = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+	int id = accept(server.server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
 	printf("Client connected\n");
 
-	// response to client
-	char *response = "HTTP/1.1 200 OK\r\n\r\n";
-	int bytes_send = send(id, response, strlen(response), 0);
+	uint8_t request_buffer[MAX_HEADER_SIZE + MAX_BODY_SIZE + MAX_URL_SIZE + MAX_EXTRA_SIZE];
+	int bytes_received = recv(id, request_buffer, sizeof(request_buffer), 0);
+
+	struct http_request *req = parse_http_request(request_buffer, bytes_received);
+
+	if (req->method == GET && strcmp(req->url, "/") == 0) {
+		send(id, ok_res_str, strlen(ok_res_str), 0);
+	} else {
+		send(id, not_found_res_str, strlen(not_found_res_str), 0);
+	}
 
 	// close the connection
-	close(server_fd);
+	free_http_request(req);
+	free_http_response(ok_res);
+	free_http_response(not_found_res);
+	free(ok_res_str);
+	free(not_found_res_str);
+	free_http_server(&server);
 
 	return 0;
 }
